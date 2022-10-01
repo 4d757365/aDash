88ce3f93791914f156ff7d85ed6c397b446f1fbe
Muse Tesfaye <tesfaye.musee@gmail.com>
2022-10-01 16:26:21 -0400

    Create user page
    User single page to view user info.

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
+import Sidebar from '../../components/sidebar/Sidebar';
+import Navbar from '../../components/navbar/Navbar';
+import Chart from '../../components/chart/Chart';
+import List from '../../components/list/List';
 import './single.scss'
+
 const Single = () => {
   return (
-    <div className="list">
-        Single
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img src="https://images.unsplash.com/photo-1506748566756-07746caecd61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="" className='itemImg' />
+              <div className="details">
+                <h1 className="itemTitle">John Smith</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email: </span>
+                  <span className="itemValue">johnsmith@gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone: </span>
+                  <span className="itemValue">+1 (123) 456-789</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address: </span>
+                  <span className="itemValue">16 East Street New York, NY</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country: </span>
+                  <span className="itemValue">United States</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <Chart aspect={3 / 1} title="User Spendings (Last 6 Months)"/>
+          </div>
+        </div>
+        <div className="bottom">
+          <h1 className="title">Orders</h1>
+          <List/>
+        </div>
+      </div>
     </div>
   )
 }
