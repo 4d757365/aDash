d9325748c438f555f016d24974a304b2bce80702
Muse Tesfaye <tesfaye.musee@gmail.com>
2022-10-02 15:10:50 -0400

    Add router

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,19 +1,32 @@
 import './datatable.scss'
 import { DataGrid } from '@mui/x-data-grid';
-import {columns, rows} from '../../data';
+import { columns, rows } from '../../data';
+import { Link } from 'react-router-dom';
 
 const Datatable = () => {
-    const actionColumn = [{field:'action', headerName:'Action', width: 200, renderCell:()=>{
-        return(
-            <div className='action'>
-                <div className="view">View</div>
-                <div className="delete">Delete</div>
+  const actionColumn = [{
+    field: 'action', headerName: 'Action', width: 200, renderCell: () => {
+      return (
+        <div className='action'>
+          <Link to='/users/test' style={{textDecoration:"none"}}>
+            <div className="view">
+              View
             </div>
-        )
-    }}];
+          </Link>
+          <div className="delete">Delete</div>
+        </div>
+      )
+    }
+  }];
   return (
     <div className='datatable'>
-        <DataGrid
+      <div className="title">
+        Add New User 
+        <Link to='/users/new' style={{textDecoration:"none"}} className="link">
+          Add New
+        </Link>
+      </div>
+      <DataGrid
         rows={rows}
         columns={columns.concat(actionColumn)}
         pageSize={9}
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -11,64 +11,74 @@ import HistoryOutlinedIcon from '@mui/icons-material/HistoryOutlined';
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
     return (
         <div className='sidebar'>
-            <div className="top"><span className="logo">aDash</span></div>
+            <div className="top">
+                <Link to="/" style={{textDecoration:"none"}}><span className="logo">aDash</span></Link>
+            </div>
+
             <div className="line"></div>
             <div className="center">
                 <ul>
                     <p className="title">MAIN</p>
+                    <Link to="/" style={{textDecoration:"none"}}>
                     <li>
-                        <GridViewIcon className='icon'/>
+                        <GridViewIcon className='icon' />
                         <span>Dashboard</span>
                     </li>
+                    </Link>
+                    <Link to="/users" style={{textDecoration:"none"}}>
                     <li>
-                        <PeopleOutlineIcon className='icon'/>
+                        <PeopleOutlineIcon className='icon' />
                         <span>Users</span>
                     </li>
+                    </Link>
+                    <Link to="/products" style={{textDecoration:"none"}}>
                     <li>
-                        <StoreOutlinedIcon className='icon'/>
+                        <StoreOutlinedIcon className='icon' />
                         <span>Products</span>
                     </li>
+                    </Link>
                     <li>
-                        <CreditCardOutlinedIcon className='icon'/>
+                        <CreditCardOutlinedIcon className='icon' />
                         <span>Orders</span>
                     </li>
                     <li>
-                        <LocalShippingOutlinedIcon className='icon'/>
+                        <LocalShippingOutlinedIcon className='icon' />
                         <span>Delivery</span>
                     </li>
                     <p className="title">USEFUL</p>
                     <li>
-                        <InsightsOutlinedIcon className='icon'/>
+                        <InsightsOutlinedIcon className='icon' />
                         <span>Stats</span>
                     </li>
                     <li>
-                        <NotificationsOutlinedIcon className='icon'/>
+                        <NotificationsOutlinedIcon className='icon' />
                         <span>Notifications</span>
                     </li>
                     <p className="title">SERVICES</p>
                     <li>
-                        <CloudOutlinedIcon className='icon'/>
+                        <CloudOutlinedIcon className='icon' />
                         <span>System Health</span>
                     </li>
                     <li>
-                        <HistoryOutlinedIcon className='icon'/>
+                        <HistoryOutlinedIcon className='icon' />
                         <span>Logs</span>
                     </li>
                     <li>
-                        <SettingsOutlinedIcon className='icon'/>
+                        <SettingsOutlinedIcon className='icon' />
                         <span>Settings</span>
                     </li>
                     <p className="title">USER</p>
                     <li>
-                        <AccountCircleOutlinedIcon className='icon'/>
+                        <AccountCircleOutlinedIcon className='icon' />
                         <span>Profile</span>
                     </li>
                     <li>
-                        <LogoutOutlinedIcon className='icon'/>
+                        <LogoutOutlinedIcon className='icon' />
                         <span>Logout</span>
                     </li>
                 </ul>
